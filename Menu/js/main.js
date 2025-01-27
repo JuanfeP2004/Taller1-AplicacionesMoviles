3dba6fa875efdfebf8597435a8f495252aa1fee1
JuanfeP2004 <juanf.ramirezc@upb.edu.co>
2025-01-27 07:31:24 -0500

    Se arreglo el scroll usando JavaScript

--- Menu/js/main.js
+++ Menu/js/main.js
@@ -1,5 +1,45 @@
 document.getElementById("link-home").addEventListener("click", function() {
+    console.log("Click en Home");
     const destino = document.getElementById("home");
+    const offset = 100;
+
+    const posicion = destino.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+        top: posicion,
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("link-about").addEventListener("click", function() {
+    console.log("Click en Home");
+    const destino = document.getElementById("about");
+    const offset = 100;
+
+    const posicion = destino.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+        top: posicion,
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("link-services").addEventListener("click", function() {
+    console.log("Click en Home");
+    const destino = document.getElementById("services");
+    const offset = 100;
+
+    const posicion = destino.getBoundingClientRect().top + window.scrollY - offset;
+
+    window.scrollTo({
+        top: posicion,
+        behavior: "smooth"
+    });
+});
+
+document.getElementById("link-contact").addEventListener("click", function() {
+    console.log("Click en Home");
+    const destino = document.getElementById("contact");
     const offset = 100;
 
     const posicion = destino.getBoundingClientRect().top + window.scrollY - offset;
